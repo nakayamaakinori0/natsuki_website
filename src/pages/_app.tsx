de6fbd27cf78749f0a37059afb359f36a2786f42
Ak norinori <42559915+nakayamaakinori0@users.noreply.github.com>
2024-08-25 13:13:14 +0800

    add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/components/Layout";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -8,6 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Layout>
         <Component {...pageProps} />
       </Layout>
+      <GoogleTagManager gtmId="G-FGZ6FF3PZG" />
     </div>
   );
 }
