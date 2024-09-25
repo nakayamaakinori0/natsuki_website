77f0f06191b06bd655bbcdd5bc6ace56b1739c74
Ak norinori <42559915+nakayamaakinori0@users.noreply.github.com>
2024-09-26 00:07:11 +0900

    add sns link config

---
+++ src/libs/config.ts
@@ -0,0 +1,5 @@
+const config = {
+  YOUTUBE_SNS_URL: "https://www.youtube.com/@user-gj3ez3dc5y/videos",
+  INSTAGRAM_SNS_URL: "https://www.instagram.com/natsuki_nakayama/",
+};
+export default config;
