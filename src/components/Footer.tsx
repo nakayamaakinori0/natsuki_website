09c54bd368ff3bd869053558b05ed727fab48e81
Ak norinori <42559915+nakayamaakinori0@users.noreply.github.com>
2024-09-26 00:06:37 +0900

    add footer sns link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import Link from "next/link";
 import Signature from "./Signature";
+import config from "@/libs/config";
 
 function Footer() {
   return (
     <footer className="px-5 mt-6 border flex justify-between gap-2 ">
-      <div className="flex  items-center gap-2">
+      <div className="flex items-center gap-2">
         <Link href="/" className="hover:bg-accent">
           <div className="flex">
             <div className="i-simple-icons-facebook w-10 h-10"></div>
           </div>
         </Link>
-        <Link href="/" className="hover:bg-accent">
+        <Link href={config.INSTAGRAM_SNS_URL} className="hover:bg-accent">
           <div className="flex">
             <div className="i-simple-icons-instagram w-10 h-10"></div>
           </div>
@@ -21,7 +22,7 @@ function Footer() {
             <div className="i-simple-icons-x w-10 h-10"></div>
           </div>
         </Link>
-        <Link href="/" className="hover:bg-accent">
+        <Link href={config.YOUTUBE_SNS_URL} className="hover:bg-accent">
           <div className="flex">
             <div className="i-simple-icons-youtube w-10 h-10"></div>
           </div>
