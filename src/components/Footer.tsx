ef42c1b300bcc0c82cebb45192c732f9a714663a
Ak norinori <42559915+nakayamaakinori0@users.noreply.github.com>
2024-11-09 21:52:38 +0900

    sns linkをblankにする

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,17 +7,29 @@ function Footer() {
   return (
     <footer className="px-5 mt-6 border flex justify-between gap-2 ">
       <div className="flex items-center gap-2">
-        <Link href={config.INSTAGRAM_SNS_URL} className="hover:bg-accent">
+        <Link
+          target="_blank"
+          href={config.INSTAGRAM_SNS_URL}
+          className="hover:bg-accent"
+        >
           <div className="flex">
             <div className="i-simple-icons-instagram w-10 h-10"></div>
           </div>
         </Link>
-        <Link href={config.X_SNS_URL} className="hover:bg-accent">
+        <Link
+          target="_blank"
+          href={config.X_SNS_URL}
+          className="hover:bg-accent"
+        >
           <div className="flex">
             <div className="i-simple-icons-x w-10 h-10"></div>
           </div>
         </Link>
-        <Link href={config.YOUTUBE_SNS_URL} className="hover:bg-accent">
+        <Link
+          target="_blank"
+          href={config.YOUTUBE_SNS_URL}
+          className="hover:bg-accent"
+        >
           <div className="flex">
             <div className="i-simple-icons-youtube w-10 h-10"></div>
           </div>
